aa700cd7db373172212d7b9f74dcef3a8736be9f
M <m@bass.si>
2021-01-18 07:17:50 +0100

    dodana neka funkciolanost + ingress deployment

--- src/main/java/me/eminem/externalRSO/StorageController.java
+++ src/main/java/me/eminem/externalRSO/StorageController.java
@@ -1,6 +1,7 @@
 package me.eminem.externalRSO;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +20,24 @@ public class StorageController {
         this.taskService = taskService;
     }
 
-
-
-    @GetMapping(path="/isPokemon/{pokename}")
-    public String isPokemon(@PathVariable String pokename){
+    @CrossOrigin(origins = "*")
+    @GetMapping(path="/returnPokemon/{pokename}")
+    public String returnPokemon(@PathVariable String pokename){
+        return taskService.returnPokemon(pokename);
+    }
+    @CrossOrigin(origins = "*")
+    @GetMapping(path="/getPokemon/{pokename}")
+    public String getPokemon(@PathVariable String pokename){
         return taskService.getPokemon(pokename);
+    }
+    @CrossOrigin(origins = "*")
+    @GetMapping(path="/isPokemon/{pokename}")
+    public boolean isPokemon(@PathVariable String pokename){
+        return taskService.isPokemon(pokename);
+    }
+    @CrossOrigin(origins = "*")
+    @GetMapping(path="/getPicPokemon/{pokename}")
+    public String getPicPokemon(@PathVariable String pokename){
+        return taskService.getPicPokemon(pokename);
     }
 }
--- src/main/java/me/eminem/externalRSO/TaskService.java
+++ src/main/java/me/eminem/externalRSO/TaskService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class TaskService {
@@ -18,11 +20,43 @@ public class TaskService {
         return taskRepository.findAll();
     }
 
-    String getPokemon(String pokename){
+    String returnPokemon(String pokename){
         RestTemplate restTemplate = new RestTemplate();
         String fooResourceUrl
                 = "https://pokeapi.co/api/v2/pokemon/"+pokename;
         ResponseEntity<String> response = restTemplate.getForEntity(fooResourceUrl, String.class);
         return response.getBody();
+    }
+
+    String getPokemon(String pokename){
+        return "place holder tuki";
+    }
+
+    boolean isPokemon(String pokename){
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            String fooResourceUrl
+                    = "https://pokeapi.co/api/v2/pokemon/" + pokename;
+            ResponseEntity<String> response = restTemplate.getForEntity(fooResourceUrl, String.class);
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
+
+    String getPicPokemon(String pokename){
+        RestTemplate restTemplate = new RestTemplate();
+        String fooResourceUrl
+                = "https://pokeapi.co/api/v2/pokemon/"+pokename;
+        ResponseEntity<String> response = restTemplate.getForEntity(fooResourceUrl, String.class);
+        String stringec = response.getBody();
+
+        String wordToFind = "official-artwork";
+        int position = stringec.indexOf(wordToFind);
+        int positionEnd = stringec.indexOf(".png\"}}",position+wordToFind.length());
+
+        String url = stringec.substring(position+36, positionEnd+4);
+
+        return url;
     }
 }
